7277c2289e50c9a4521c23a8b18c06f510996dc4
vigisbig Vig <>
2022-08-24 19:43:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+first_number = input ("Enter first number - ")
+second_number = input ("Enter second number - ")
+answer = int(first_number) + int(second_number)
+answer_string = str (answer) # answer converted to string
+print("The answer is " + answer_string)
