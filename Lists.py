123a505173d6084a94d7d8ca782cb409222f7a73
vigisbig Vig <vigisbig@gmail.com>
2022-08-25 18:55:31 +0000

    New file Lists added

---
+++ Lists.py
@@ -0,0 +1,12 @@
+names = ["John", "Bob", "Mon", "Mary"]      #    Boolean, Numbers (Integer and Float), String are primitive
+                                            #    or basic data types, we are now discussing lists which are complex data type.
+print(names)                                #    We use list when we want to represent a list of objects,
+                                            #    like list of names or list of numbers etc.
+print(names[2])
+print(names[-1])                            #    There's a provision to use negative index in Python
+
+names [0] = "Jon"                           #    This way, we can change variable inside list
+
+print (names)
+
+print(names[0:3])                           # to display a range of values inside list
