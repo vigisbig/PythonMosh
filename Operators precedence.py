353b03e85c220a81c40c27c91ab8dc4944ebe423
vigisbig Vig <vigisbig@gmail.com>
2022-08-25 09:26:14 +0000

    Added info about operator precedence and how it impacts calculation. Bracket can be used to change precedence and the code in bracket will be calculated first

---
+++ Operators precedence.py
@@ -0,0 +1,8 @@
+x = 10 + 3 * 2  # first multiplication is performed and then addition
+
+print(x)
+
+y = (10+3)*2    # bracket can be used to change the operator precedence. The one is bracket is executed first
+                # in this case
+
+print(y)
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-print (10 + 9)
-print (90 - 34)
-print (2 * 3)
-print (10 / 3) # returns float
-print (10 // 3) # returns integer
-print (10 % 3) # modulus operator, and this provides the remainder value
-print (10 ** 3) # exponent operator, i.e 10^3
+x = 10 + 3 * 2  # first multiplication is performed and then addition
 
-x = 10 # this is a use of assignment an operator that assigns value of 10 to the variable X
+print(x)
 
-#we can increment the value of x by 3 using following code
+y = (10+3)*2    # bracket can be used to change the operator precedence. The one is bracket is executed first
+                # in this case
 
-x = x+3 # this updates the value of x to 13
-
-x+=3 # this is an alternative way of doing the above. this is called as augmented assignment operator.
-
-x-=3 # this is an augmented assignment operator too which decreases the value by 3
-
-x*=3 # works same way as above
+print(y)
