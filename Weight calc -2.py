8bce0731e01e98f0b26d46176c42c62453985058
vigisbig Vig <vigisbig@gmail.com>
2022-08-25 17:59:57 +0000

    Added new file for while loops

--- Weight calc -2.py
+++ Weight calc -2.py
@@ -1,15 +1,15 @@
-weight = int(input("What is your weight? "))
-unit = input("(K)g or (L)b?" )
+weight = float(input("What is your weight? "))
+unit = input("(K)g or (L)b? ")
 
-if unit.upper == "K":
+if unit.upper() == "K":
 
-  WP = float(weight)/0.45
+  WP = weight/0.45
   
-  print("Weight in pound is "+ str(WP)"Pound")
+  print("Weight in pound is "+ str(WP)+"Pound")
 
   
 else:
 
-  WK = float(weight)*0.45
+  WK = weight*0.45
   
-  print("Weight in kg is "+ str(WK)"Kilogram")
+  print("Weight in kg is "+ str(WK)+"Kilogram")
